7c2eaf65d2c1cd32d6ddcf36cddddf28cbe99369
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-08 13:26:19 +0700

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const webpackConfig = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  }
+};
+
+module.exports = webpackConfig;
