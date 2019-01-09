df4eea9fd943b4e5fd2bcd9d9d0e21e62cef76a5
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-09 08:45:04 +0700

    Add webpack loader

--- webpack/configuration/module.js
+++ webpack/configuration/module.js
@@ -7,5 +7,25 @@ export default {
         loader: 'babel-loader',
       },
     },
+    {
+      test: /\.scss$/,
+      use: [
+        {
+          loader: 'style-loader',
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            importLoaders: 1,
+            localIdentName: '[path][name]__[local]--[hash:base64:5]',
+            sourceMap: true,
+          },
+        },
+        {
+          loader: 'sass-loader',
+        },
+      ],
+    },
   ],
 };
