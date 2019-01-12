701d92090e940bdec48686fbedeb3e5a90f4ec66
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-12 18:27:13 +0700

    Refactor client app add routes

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,29 +1,19 @@
 import React from 'react';
-import {
-  BrowserRouter, Route, StaticRouter, Switch,
-} from 'react-router-dom';
+import { BrowserRouter, StaticRouter } from 'react-router-dom';
 
-import About from '@components/About';
-import Home from '@components/Home';
+import routes from '../shared/routes';
 
 export default ({ server, location, context = {} }) => {
-  const routes = (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-    </Switch>
-  );
-
   let router = (
     <BrowserRouter>
-      {routes}
+      {routes.all}
     </BrowserRouter>
   );
 
   if (server) {
     router = (
       <StaticRouter location={location} context={context}>
-        {routes}
+        {routes.all}
       </StaticRouter>
     );
   }
