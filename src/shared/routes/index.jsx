15f1e8035854f696588d1a838615e7e4f204425c
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-14 21:23:47 +0700

    Refactor routes add calculator

--- src/shared/routes/index.jsx
+++ src/shared/routes/index.jsx
@@ -1,33 +1,19 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import About from '@about';
-import Home from '@home';
-import Todo from '@todo';
+import Calculator from '../../client/calculator';
 
 const paths = [
   {
-    component: Home,
+    component: Calculator,
     exact: true,
     path: '/',
-  },
-  {
-    component: About,
-    exact: true,
-    path: '/about',
-  },
-  {
-    component: Todo,
-    exact: true,
-    path: '/todo',
   },
 ];
 
 const all = (
   <Switch>
     <Route exact path={paths[0].path} component={paths[0].component} />
-    <Route exact path={paths[1].path} component={paths[1].component} />
-    <Route exact path={paths[2].path} component={paths[2].component} />
   </Switch>
 );
 
